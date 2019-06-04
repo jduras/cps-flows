c6db9116f837cf1d00d92a47bd6e376f4a6b7726
jduras <jan.duras@gmail.com>
2019-06-04 10:43:40 +0200

    clean up, speed up

--- code/c14_construct_shares_agg_lom.R
+++ code/c14_construct_shares_agg_lom.R
@@ -1,128 +1,146 @@
 
-print("Constructing population shares using law of motion")
-
 # construct lfs population shares for counterfactual transition rates using law of motion
 
 # population shares from construct_stocks_agg.R
-load(file = paste0(edir.cps, "stocksandshares_agg.Rdata"))
+load(file = str_c(edir_cps, "out_stocksandshares_agg.Rdata"))
 # transition rates from construct_rates_2m_agg_counter.R
-load(file = paste0(edir.cps, "rates_agg_counter.Rdata"))
+load(file = str_c(edir_cps, "out_rates_agg_counter.Rdata"))
 
-chosen.source <- "CPS"
-chosen.seas <- "NSA"
-chosen.measure <- "shr.lfs2pop"
+chosen_source <- "CPS"
+chosen_seas <- "SA"
+chosen_measure <- "shr_lfs2pop"
 
-# period from which the law of motion will start, for chosen.source == BLS this has to be 199001 or later
+# period from which the law of motion will start, for chosen_source == BLS this has to be 199001 or later
 tinitial <- 197601
 # tinitial <- 198001
 # tinitial <- 200712
-if (chosen.source == "BLS") tinitial <- max(tinitial, 199001)
+if (chosen_source == "BLS") tinitial <- max(tinitial, 199001)
 
-# in case of the actual.rate scenario with CPS NSA rates use transition rates from year ago
+# in case of the actual_rate scenario with CPS NSA rates use transition rates from year ago
 # for periods where it is not possible to match consequitive months in CPS data
-if (chosen.source == "CPS" & chosen.seas == "NSA")
-    df.rates.all.agg.counter.adj %<>%
+if (chosen_source == "CPS" & chosen_seas == "NSA")
+    df_rates_all_agg_counter_adj %<>%
     group_by(source, seas, scenario) %>%
-    mutate(rate = if_else(source == "CPS" & seas == "NSA" & scenario == "actual.rate" &
-                              period.1 %in% c(197712, 198506, 198509, 199312, 199505, 199506, 199507, 199508),
+    mutate(rate = if_else(source == "CPS" & seas == "NSA" & scenario == "actual_rate" &
+                              period_1 %in% c(197712, 198506, 198509, 199312, 199505, 199506, 199507, 199508),
                           lag(rate, 12), rate)) %>%
     ungroup()
 
 # actual data and dates for which the lom will be simulated
-df.shares.agg <-
-    df.stocksandshares.all.agg.whole.sample %>%
+df_shares_agg <-
+    df_stocksandshares_all_agg_whole_sample %>%
     filter(lfs != "M") %>%
-    gather(measure, value, c(s, shr.lfs2pop)) %>%
-    filter(measure == chosen.measure) %>%
-    filter(source == chosen.source) %>%
-    filter(seas == chosen.seas) %>%
+    gather(measure, value, c(s, shr_lfs2pop)) %>%
+    filter(measure == chosen_measure) %>%
+    filter(source == chosen_source) %>%
+    filter(seas == chosen_seas) %>%
     filter(period >= tinitial) %>%
     select(source, period, seas, measure, lfs, value)
 
 dates <-
-    df.shares.agg %>%
+    df_shares_agg %>%
     spread(lfs, value) %>%
     select(period) %>%
     as.matrix()
 
-# initialize results of simulation
-sim.results <- list()
-
-sim.init <-
+sim_init <-
     matrix(NA_real_, length(dates), 3) %>%
     set_rownames(dates) %>%
     set_colnames(c("E", "I", "U"))
 
-sim.init[1, ] <-
-    df.shares.agg %>%
+sim_init[1, ] <-
+    df_shares_agg %>%
     filter(period == tinitial) %>%
     spread(lfs, value) %>%
     select(E, I, U) %>%
     as.matrix()
 
-# simulations <- c("actual.rate", "avg.rate")
-simulations <- c("actual.rate", "avg.rate", "counter1", "counter2", "counter3", "counter4", "counter5")
+# simulations <- c("actual_rate", "avg_rate")
+simulations <- c("actual_rate", "avg_rate", "counter1", "counter2", "counter3", "counter4", "counter5")
 
-for (simulation in simulations) {
+# initialize results of simulation
+sim_results <- vector("list", length(simulations))
 
-    sim <- sim.init
-    t1 <- tinitial
+tic()
 
-    # loop for law of motion
-    while (t1 < tlast) {
-        t2 <- if_else(t1 %% 100 == 12, t1 + 89, t1 + 1)
+message("Constructing population shares using law of motion")
 
-        print(paste("Constructing shares using law of motion:  scenario =", simulation, " date =", t1))
+sim_results <-
+    future_map_dfr(
+        .x = set_names(simulations),
+        .id = "scenario",
+        .f = function(simulation = .x) {
 
-        # transition matrix
-        trans.matrix <-
-            df.rates.all.agg.counter.adj %>%
-            filter(source == chosen.source) %>%
-            filter(seas == chosen.seas) %>%
-            filter(scenario == simulation) %>%
-            filter(period.1 == t1) %>%
-            select(lfs.1, lfs.2, rate) %>%
-            spread(lfs.2, rate) %>%
-            as.data.frame() %>%
-            column_to_rownames("lfs.1") %>%
-            as.matrix()
+            sim <- sim_init
+            t1 <- tinitial
 
-        # construct shares using law of motion
-        sim[as.character(t2), ] <- sim[as.character(t1), ] %*% trans.matrix
+            message(str_c(" done: scenario ", simulation))
 
-        t1 <- t2
-    }
+            df_tmp <- df_rates_all_agg_counter_adj %>%
+                filter(source == chosen_source) %>%
+                filter(seas == chosen_seas) %>%
+                filter(scenario == simulation)
 
-    sim.results[[simulation]] <- sim
-}
+            # loop for law of motion
+            # note: tseq may be ordered by (month, year) instead of month+year so while is used here instead of map
+            while (t1 < tlst) {
+                t2 <- if_else(t1 %% 100 == 12, t1 + 89, t1 + 1)
+
+                # message(str_c("Constructing shares using law of motion:  scenario = ", simulation, " date = ", t1))
+
+                # transition matrix
+                trans_matrix <-
+                    df_tmp %>%
+                    filter(period_1 == t1) %>%
+                    select(lfs_1, lfs_2, rate) %>%
+                    spread(lfs_2, rate) %>%
+                    column_to_rownames("lfs_1") %>%
+                    as.matrix()
+
+                # construct shares using law of motion
+                sim[as.character(t2), ] <- sim[as.character(t1), ] %*% trans_matrix
+
+                t1 <- t2
+            }
+
+            sim %>%
+                as_tibble(rownames = "period")
+        })
+
+toc()
 
 # simulated and actual time series
-df.shares.lom.agg.counter <-
-    bind_rows(tibble(source = chosen.source,
-                     seas = chosen.seas,
-                     scenario = simulations,
-                     measure = chosen.measure,
-                     values = map(sim.results, ~.x %>% as_tibble(rownames = "period"))) %>%
-                  unnest(values) %>%
-                  gather(lfs, value, c("E", "I", "U")) %>%
-                  mutate(period = as.numeric(period)),
-              df.shares.agg %>%
-                  mutate(scenario = "actual"))
+df_shares_lom_agg_counter <-
+    bind_rows(df_shares_agg %>%
+                  mutate(scenario = "actual"),
+              sim_results %>%
+                  mutate(period = as.numeric(period),
+                         source = chosen_source,
+                         seas = chosen_seas,
+                         measure = chosen_measure) %>%
+                  select(source, period, seas, measure, everything()) %>%
+                  gather(lfs, value, c("E", "I", "U")))
 
-save(df.shares.lom.agg.counter, file = paste0(edir.cps, "shares_agg_lom_", chosen.source, "_", chosen.seas, "_", tinitial, ".Rdata"))
+save(df_shares_lom_agg_counter, file = str_c(edir_cps, "out_shares_agg_lom_", chosen_source, "_", chosen_seas, "_", tinitial, ".Rdata"))
 
-# load(file = paste0(edir.cps, "shares_agg_lom_", chosen.source,"_",chosen.seas,".Rdata"))
+# load(file = str_c(edir_cps, "out_shares_agg_lom_", chosen_source,"_",chosen_seas,".Rdata"))
 
 # plot simulated and actual shares
-df.shares.lom.agg.counter %>%
-    mutate(monyear = period %>% as.character() %>% as.yearmon("%Y%m"),
-           type = if_else(scenario == "actual", "actual", "simulated")) %>%
-    # filter(scenario %in% c("actual", "actual.rate", "avg.rate", "counter1")) %>%
-    filter(seas == chosen.seas) %>%
-    ggplot(data = .) +
-        geom_line(aes(x = monyear, y = value, col = scenario)) +
-        geom_rect(data = (rec.dates %>% filter(Start > as.yearmon(as.character(tinitial), format = "%Y%m"))),
+df_shares_lom_agg_counter %>%
+    mutate(yearm = period %>% as.character() %>% as.yearmon("%Y%m"),
+           type = if_else(scenario == "actual", "actual", "simulated"),
+           lfs_label = recode(lfs, "E" = "employed", "U" = "unemployed", "I" = "not in labor force")) %>%
+    # filter(scenario %in% c("actual", "actual_rate", "avg_rate", "counter1")) %>%
+    filter(seas == chosen_seas) %>%
+    ggplot() +
+        geom_line(aes(x = yearm, y = value, col = scenario)) +
+        geom_rect(data = (rec_dates %>% filter(Start > as.yearmon(as.character(tinitial), format = "%Y%m"))),
                   aes(xmin = Start, xmax = End, ymin = -Inf, ymax = +Inf), alpha = 0.2) +
         scale_x_yearmon() +
-        labs(x = "", y = "", title = "") +
-        facet_wrap(~ lfs, ncol = 1, scales = "free_y")
+        scale_color_manual(values = c("black", brewer.pal(length(simulations), "Set1"))) +
+        scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+        labs(x = "", y = "",
+             title = "Population Shares by Labor Force Status",
+             subtitle = "Actual vs Simulated Using Law of Motion") +
+        facet_wrap(~ lfs_label, ncol = 1, scales = "free_y") +
+        theme(strip.text = element_text(hjust = 0))
